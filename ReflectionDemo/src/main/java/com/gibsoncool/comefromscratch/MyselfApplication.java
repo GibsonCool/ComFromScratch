fcc892ea5d96b585a9cee689ad05530ba6616885
my_dell <907864082@qq.com>
2017-10-20 16:56:27 +0800

    增加文件

---
+++ ReflectionDemo/src/main/java/com/gibsoncool/comefromscratch/MyselfApplication.java
@@ -0,0 +1,11 @@
+package com.gibsoncool.comefromscratch;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/10/20.
+ */
+
+public class MyselfApplication
+        extends Application {
+}
